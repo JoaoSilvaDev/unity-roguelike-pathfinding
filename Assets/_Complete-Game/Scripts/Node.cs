75ec663ccd26fb5e923102dbcfa978fa7eabf318
Joao Silva <joaosdev@gmail.com>
2018-12-18 12:11:24 +0000

    initialized project

---
+++ Assets/_Complete-Game/Scripts/Node.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    [SerializeField] private Transform fromNode = null;
+    [SerializeField] private List<Transform> toNodes;
+}
